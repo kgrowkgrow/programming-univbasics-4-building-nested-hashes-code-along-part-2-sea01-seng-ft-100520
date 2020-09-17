c0775a3a96ad921254e1374cc76b1e57772b4bcf
Kevin Grow <kevingrow11@gmail.com>
2020-09-17 19:36:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -59,8 +59,8 @@ def add_characters
    # Write code below that does the following programmatically:
    
    epic_tragedy[:additional_characters] = 
-    {:name => "Prince Escalus"}
-    {:name => "Apothecary"}
+    [:name => "Prince Escalus"]
+    [:name => "Apothecary"]
    
    
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
